2dceda50491c5057ad783164d79a9a1dd42a570d
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2023-01-10 22:07:28 +0200

    delete unnecessary code; simplify some functions

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
-const hrs = 24;
-const min = 60;
-const sec = 60;
+
+const alarmType = ["alarmh", "alarmm", "alarms"];
+const alarmValue = [24, 60, 60];
+const alarmDiv = ["select", "button"];
+const alarmButton = ["set", "clear", "del"];
 let index = 0;
 let time;
+
 const timeZero = (zero) => {
   return (zero < 10) ? "0" + zero : zero;
 }
@@ -17,75 +20,82 @@ const currentTime = () => {
   document.getElementById("clock").innerHTML = time;
   setTimeout(currentTime, 1000);
 }
+
 currentTime();
 
-const selectOption = (select, max) => {
-	for (let i=1; i <= max; i++) {
-		select.options[select.options.length] = new Option(timeZero(i));
-	}
-}
-
-const createDiv = (...whatId) => {
-  for (let i = 0; i < whatId.length; i++){
+const createDiv = () => {
+  for (let i = 0; i < alarmDiv.length; i++){
   let smtg = document.createElement("div");
-  smtg.setAttribute("id", whatId[i] + index);
-  document.getElementById("alarm-container").appendChild(smtg);
+  smtg.setAttribute("id", alarmDiv[i] + index);
   }
+  document.getElementById("alarm-container").appendChild(smtg);
 }
 
-const createSelect = (...whatId) => {
-  for (let i = 0; i < whatId.length; i++){
+const createSelect = () => {
+  for (let i = 0; i < alarmType.length; i++){
   let smtg = document.createElement("select");
-  smtg.setAttribute("id", whatId[i] + index);
+  smtg.setAttribute("id", alarmType[i] + index);
+  for (let j=1; j <= alarmValue[i]; j++) {
+		smtg.options[j-1] = new Option(timeZero(j-1));
+	}
   document.getElementById("select" + index).appendChild(smtg);
   }
 }
-
-const createButton = (...whatId) => {
-  for (let i = 0; i < whatId.length; i++){
+const createButton = () => {
+  for (let i = 0; i < alarmButton.length; i++){
   let smtg = document.createElement("button");
-  smtg.setAttribute("id", whatId[i] + "Button" + index);
-  let upper = whatId[i].charAt(0).toUpperCase() + whatId[i].slice(1);
+  smtg.setAttribute("id", alarmButton[i] + "Button" + index);
+  let upper = alarmButton[i].charAt(0).toUpperCase() + alarmButton[i].slice(1);
   smtg.setAttribute("onClick", `alarm${upper}(getAttribute('id'))`);
+  }
   document.getElementById("button" + index).appendChild(smtg);
+}
+
+const changeStateAlarm = (id, bool) => {
+  for (let i = 0; i < alarmType.length; i++){
+  document.getElementById(alarmType[i] + id).disabled = bool;
+}
+}
+
+const getAlarmTime = (id) => {
+  let selected = "";
+  for (let i = 0; i < alarmType.length; i++){
+  let doc = document.getElementById(alarmType[i] + id);
+  selected += doc.options[doc.selectedIndex].value + ":";
+  }
+  return selected.substring(0, selected.length - 1);
+}
+
+const deleteDiv = (id) => {
+  for (let i = 0; i < alarmDiv.length; i++){
+    document.getElementById(alarmDiv[i] + id).remove();
   }
 }
 
 const alarmAdd = () => {
-  createDiv("select", "button");
-  createSelect("alarmh", "alarmm", "alarms");
-  createButton("set", "clear", "del");
-  selectOption(document.getElementById("alarmh" + index), hrs);
-  selectOption(document.getElementById("alarmm" + index), min);
-  selectOption(document.getElementById("alarms" + index), sec);
+  createDiv();
+  createSelect();
+  createButton();
   index++;
 }
+
 const alarmSet = (id) => {
   let neededId = id.replace("setButton", "");
-  document.getElementById("alarmh" + neededId).disabled = true;
-	document.getElementById("alarmm" + neededId).disabled = true;
-	document.getElementById("alarms" + neededId).disabled = true;
-  let hr = document.getElementById("alarmh" + neededId);
-  let selectedHour = hr.options[hr.selectedIndex].value;
-  let mi = document.getElementById("alarmm" + neededId);
-  let selectedMin = mi.options[mi.selectedIndex].value;
-  let se = document.getElementById("alarms" + neededId);
-  let selectedSec = se.options[se.selectedIndex].value;
-  let alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec;
-  const myInterval = setInterval(function(){
+  changeStateAlarm(neededId, true);
+  let alarmTime = getAlarmTime(neededId);
+  setInterval(function(){
   if (alarmTime == time) {
 		alert("Hello");
 	}
 },1000);
 }
+
 const alarmClear = (id) => {
   let neededId = id.replace("clearButton", "");
-  document.getElementById("alarmh" + neededId).disabled = false;
-	document.getElementById("alarmm" + neededId).disabled = false;
-	document.getElementById("alarms" + neededId).disabled = false;
+  changeStateAlarm(neededId, false);
 }
+
 const alarmDelete = (id) => {
   let neededId = id.replace("deleteButton", "");
-  document.getElementById("select" + neededId).remove();
-  document.getElementById("button" + neededId).remove();
+  deleteDiv(neededId);
 }
