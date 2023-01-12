8dd8731ce9a727bae0ef20d3811289da4ef260f9
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2023-01-13 01:08:19 +0200

    add base script to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+import { alarmSet, alarmClear, alarmDel } from "./alarmButtons.js";
+import { downloadAlarm } from "./downloadAlarm.js";
+import { uploadAlarm } from "./uploadAlarm.js";
+import { alarmAdd } from "./alarmStart.js";
+
+document.getElementById("addAlarm").addEventListener("click", alarmAdd);
+document.getElementById("downloadButton").addEventListener("click", downloadAlarm);
+document.getElementById("uploadButton").addEventListener("change", uploadAlarm);
+window.alarmSet = alarmSet;
+window.alarmClear = alarmClear;
+window.alarmDel = alarmDel;
