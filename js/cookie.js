ce65385c7b68bc35c116c526c515c05c88f6d2c8
unknown <andrii.shchegel@gmail.com>
2023-01-20 21:23:49 +0200

    name changes

--- js/cookie.js
+++ js/cookie.js
@@ -8,9 +8,9 @@ const cookie = {
 };
 
 const addCookie = () => {
-  const d = new Date();
-  d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
-  const expires = 'expires=' + d.toUTCString();
+  const date = new Date();
+  date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+  const expires = 'expires=' + date.toUTCString();
   if (!document.cookie) {
     for (let i = 0; i < cookie.name.length; i++) {
       document.cookie = cookie.name[i] + '=' + cookie.defValue[i] + ';' + expires + ';path=/';
@@ -18,16 +18,15 @@ const addCookie = () => {
     return '';
   }
   for (let sii = 0; sii < cookie.name.length; sii++) {
-    const ca = document.cookie.split('; ');
-    for (let i = 0; i < ca.length; i++) {
-      if (ca[i].indexOf(cookie.name[sii]) === 0) {
-        const c = ca[i].substring(cookie.name[sii].length + 1, ca[i].length);
-        const arr = c.split('-');
-        arr[alarmSettings.index - 1] = cookie.defValue[sii];
-        let str = '';
-        for (let j = 0; j < arr.length; j++) { str += '-' + arr[j]; }
-        document.cookie = cookie.name[sii] + '=' + str.slice(1) + ';' + expires + ';path=/';
-        break;
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+      if (cookies[i].indexOf(cookie.name[sii]) === 0) {
+        const coockieValue = cookies[i].substring(cookie.name[sii].length + 1, cookies[i].length);
+        const array = coockieValue.split('-');
+        array[alarmSettings.index - 1] = cookie.defValue[sii];
+        let string = '';
+        for (let j = 0; j < array.length; j++) string += '-' + array[j];
+        document.cookie = cookie.name[sii] + '=' + string.slice(1) + ';' + expires + ';path=/';
       }
     }
   }
@@ -35,17 +34,17 @@ const addCookie = () => {
 
 const setCookie = (id, nameId, value) => {
   const neededId = id.replace(/[a-zA-Z]+/, '');
-  const ca = document.cookie.split('; ');
-  const d = new Date();
-  d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
-  const expires = 'expires=' + d.toUTCString();
-  for (let i = 0; i < ca.length; i++) {
-    if (ca[i].indexOf(cookie.name[nameId]) === 0) {
-      const c = ca[i].substring(cookie.name[nameId].length + 1, ca[i].length);
-      const arr = c.split('-');
+  const cookies = document.cookie.split('; ');
+  const date = new Date();
+  date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+  const expires = 'expires=' + date.toUTCString();
+  for (let i = 0; i < cookies.length; i++) {
+    if (cookies[i].indexOf(cookie.name[nameId]) === 0) {
+      const coockieValue = cookies[i].substring(cookie.name[nameId].length + 1, cookies[i].length);
+      const arr = coockieValue.split('-');
       arr[neededId] = value;
       let str = '';
-      for (let j = 0; j < arr.length; j++) { str += '-' + arr[j]; }
+      for (let j = 0; j < arr.length; j++) str += '-' + arr[j];
       document.cookie = cookie.name[nameId] + '=' + str.slice(1) + ';' + expires + ';path=/';
       break;
     }
@@ -70,12 +69,10 @@ const alarmCookie = () => {
           if (sii === 0) {
             alarmAdd();
             document.getElementById('name' + j).innerHTML = size[j];
-          }
-          if (sii === 1) {
+          } else if (sii === 1) {
             const arr = size[j].split(':');
             setSelected(arr, j);
-          }
-          if (sii === 2) {
+          } else if (sii === 2) {
             if (size[j] === 'true') {
               alarmSet('setButton' + j);
             }
@@ -87,7 +84,7 @@ const alarmCookie = () => {
 };
 
 const checkCookie = () => {
-  if (document.cookie === '') { return ''; }
+  if (document.cookie === '') return '';
   alarmCookie();
 };
 
