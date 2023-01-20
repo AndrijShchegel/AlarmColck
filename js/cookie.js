54f3916e9b17216051c6d879d4d87d5465748e2a
unknown <andrii.shchegel@gmail.com>
2023-01-20 13:04:57 +0200

    add cookie reader

--- js/cookie.js
+++ js/cookie.js
@@ -1,3 +1,7 @@
+import { alarmSettings } from './config.js';
+import { alarmAdd } from './alarmStart.js';
+import { alarmSet } from './alarmButtons.js';
+
 const cookie = {
   name: ['name', 'time', 'isSet'],
   defValue: ['Alarm', '00:00:00', 'false']
@@ -22,4 +26,44 @@ const setCookie = (id, nameId, value) => {
   }
 };
 
-export {setCookie}
+const setSelected = (arr, id) => {
+    for (let j = 0; j < alarmSettings.timeUnits.length; j++) {
+      const doc = document.getElementById(alarmSettings.timeUnits[j] + id);
+      doc.selectedIndex = parseInt(arr[j]);
+    }
+  };
+
+const alarmCookie = () => {
+const ca = document.cookie.split('; ');
+for (let sii = 0; sii < cookie.name.length; sii++) {
+    for (let i = 0; i < ca.length; i++) {
+    if (ca[i].indexOf(cookie.name[sii]) === 0) {
+        const c = ca[i].substring(cookie.name[sii].length + 1, ca[i].length);
+        const size = c.split('-');
+        for (let j = 0; j < size.length; j++) {
+        if (sii === 0) {
+            alarmAdd();
+            document.getElementById('name' + j).innerHTML = size[j];
+        }
+        if (sii === 1) {
+            const arr = size[j].split(':');
+            setSelected(arr, j);
+        }
+        if (sii === 2) {
+            if (size[j] === 'true') {
+            alarmSet('setButton' + j);
+            }
+        }
+        }
+    }
+    }
+    break;
+  }
+};
+
+const checkCookie = () => {
+  if (document.cookie === '') { return ''; }
+  alarmCookie();
+};
+
+export {setCookie, checkCookie}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 import { alarmSet, alarmClear, alarmDel, alarmChangeName } from './alarmButtons.js';
 import { alarmAdd } from './alarmStart.js';
+import { setCookie, checkCookie } from './cookie.js';
 
 document.getElementById('addAlarm').addEventListener('click', alarmAdd);
 window.alarmChangeName = alarmChangeName;
 window.alarmSet = alarmSet;
+window.setCookie = setCookie;
+window.checkCookie = checkCookie;
 window.alarmClear = alarmClear;
 window.alarmDel = alarmDel;
